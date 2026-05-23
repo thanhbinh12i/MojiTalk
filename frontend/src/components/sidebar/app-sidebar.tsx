d946296b6dc0496312527b566fa332c3755a93f7
thanhbinh12i <nptbinh17092004@gmail.com>
2026-05-23 17:21:13 +0700

    set up shadcn ui

---
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,89 @@
+import { NavUser } from "@/components/sidebar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Moon, Sun } from "lucide-react";
+import { Switch } from "../ui/switch";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useThemeStore } from "@/stores/useThemeStore";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { isDark, toggleTheme } = useThemeStore();
+  const { user } = useAuthStore();
+
+  return (
+    <Sidebar variant="inset" {...props}>
+      {/* Header */}
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              size="lg"
+              asChild
+              className="bg-gradient-primary"
+            >
+              <a href="#">
+                <div className="flex w-full items-center px-2 justify-between">
+                  <h1 className="text-xl font-bold text-white">Moji</h1>
+                  <div className="flex items-center gap-2">
+                    <Sun className="size-4 text-white/80" />
+                    <Switch
+                      checked={isDark}
+                      onCheckedChange={toggleTheme}
+                      className="data-[state=checked]:bg-background/80"
+                    />
+                    <Moon className="size-4 text-white/80" />
+                  </div>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent className="beautiful-scrollbar">
+        {/* New Chat */}
+        <SidebarGroup>
+          <SidebarGroupContent>{/* <CreateNewChat /> */}</SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* Group Chat */}
+        <SidebarGroup>
+          <div className="flex items-center justify-between">
+            <SidebarGroupLabel className="uppercase">
+              nhóm chat
+            </SidebarGroupLabel>
+            {/* <NewGroupChatModal /> */}
+          </div>
+
+          <SidebarGroupContent>
+            {/* {convoLoading ? <ConversationSkeleton /> : <GroupChatList />} */}
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* Dirrect Message */}
+        <SidebarGroup>
+          <SidebarGroupLabel className="uppercase">bạn bè</SidebarGroupLabel>
+          <SidebarGroupAction title="Kết Bạn" className="cursor-pointer">
+            {/* <AddFriendModal /> */}
+          </SidebarGroupAction>
+
+          <SidebarGroupContent>
+            {/* {convoLoading ? <ConversationSkeleton /> : <DirectMessageList />} */}
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      {/* Footer */}
+      <SidebarFooter>{user && <NavUser user={user} />}</SidebarFooter>
+    </Sidebar>
+  );
+}
--- frontend/src/types/store.ts
+++ frontend/src/types/store.ts
@@ -12,10 +12,16 @@ export interface AuthState {
     password: string,
     email: string,
     firstName: string,
-    lastName: string
+    lastName: string,
   ) => Promise<void>;
   signIn: (username: string, password: string) => Promise<void>;
   signOut: () => Promise<void>;
   fetchMe: () => Promise<void>;
   refresh: () => Promise<void>;
+}
+
+export interface ThemeState {
+  isDark: boolean;
+  toggleTheme: () => void;
+  setTheme: (dark: boolean) => void;
 }
